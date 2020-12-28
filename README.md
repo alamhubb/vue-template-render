ef25211fbe2e07397b8fd97fb9a61128e51621af
qinkaiyuan <qinky94@163.com>
2020-12-28 13:49:05 +0800

    初始化代码

--- README.md
+++ README.md
@@ -4,25 +4,15 @@
 将vue的template语法字符串渲染为dom，返回dom，可获得html
 
 ### introduce
+将vue的template语法字符串渲染为dom，返回dom，可获得html
 Render the vue template syntax string as dom, return dom, and get html
-
-### 演示项目参考
-* [qing-markdown 演示项目](https://github.com/qinkaiyuan/qing-markdown#%E6%BC%94%E7%A4%BA%E9%A1%B9%E7%9B%AE)
 
 ### npm 安装
 ```bash
-npm install qing-markdown-loader -D
+npm install vue-template-render
 ```
 
 ### 快速上手
-```bash
-# 配置 loader，vue-cli 方式，vue.config.js 文件
-chainWebpack: config => {
-  config.module
-    .rule('md').test(/\.md$/).use('md-loader').loader('qing-markdown-loader')
-}
-```
-
 ```javascript
 // hello.md文件内内容为：# hello world
 import hello from './hello.md'
@@ -30,6 +20,3 @@ import hello from './hello.md'
 //# 输出 # hello world
 console.log(hello)
 ```
-
-### 结合其他项目使用
-* [qing-markdown](https://github.com/qinkaiyuan/qing-markdown#qing-markdown) Vue Markdown 组件，可以将本地或远程 .md 文件以及 markdown 字符串解析为 HTML 显示
---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+exports.__esModule = true;
+var vue_1 = require("vue");
+var TemplateRender = /** @class */ (function () {
+    function TemplateRender() {
+    }
+    TemplateRender.render = function (template, options) {
+        if (template === void 0) { template = ''; }
+        var htmlVue = new vue_1["default"](__assign(__assign({ el: document.createElement('div') }, options), { template: template }));
+        return htmlVue.$el;
+    };
+    TemplateRender.renderOptions = function (options) {
+        var htmlVue = new vue_1["default"](__assign({ el: document.createElement('div') }, options));
+        return htmlVue.$el;
+    };
+    return TemplateRender;
+}());
+exports["default"] = TemplateRender;
